3a3c7bdea9799ab7ac850e33a5c372fd30143fda
Kristina <cyberkitty628@gmail.com>
2021-02-14 23:51:10 -0800

    Finished section 1

--- dog.py
+++ dog.py
@@ -1,1 +1,15 @@
-#this is a test
+class Dog:
+    
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+        print("dog initialized!")
+
+    def bark(self):
+        print("Woof!")
+    
+    def sit(self):
+        print(self.name + " sits.")
+
+    def roll(self):
+        print(self.name + " rolls over.")
---
+++ my_dogs.py
@@ -0,0 +1,14 @@
+from dog import Dog
+
+dog_1 = Dog("Matcha", "Shiba Inu")
+dog_1.bark()
+
+dog_2 = Dog("Snow", "Samoyed")
+print(dog_2.name)
+
+dog_3 = Dog("Taro", "Pitbull")
+print(dog_3.breed)
+
+dog_1.sit()
+dog_2.roll()
+dog_3.bark()
